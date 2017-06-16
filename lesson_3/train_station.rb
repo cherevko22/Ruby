c2a8a111ec0b745d08d319e444055e65d77a4068
Sergii Cherevko <cherevko22@gmail.com>
2017-06-15 23:40:52 -0500

    Fixed

--- lesson_3/train_station.rb
+++ lesson_3/train_station.rb
@@ -1,17 +1,17 @@
 #####################################################---Station---########################################################################
 class Station
 
-  attr_reader :station_name, :list_of_trains
+  attr_reader :name, :list_of_trains
 
-  def initialize(station_name)
-    @station_name = station_name
+  def initialize(name)
+    @name = name
     @list_of_trains = []
   end
 
   #Add a train to the list of trains
   def add_train(train)
     self.list_of_trains << train
-    puts "The train number #{train.number} has come to the station #{self.station_name}"
+    puts "The train number #{train.number} has come to the station #{self.name}"
   end
 
   # Print the list of trains
@@ -30,7 +30,7 @@ class Station
   def train_departure(departured_train)
     puts "Enter the number of the train for the next departure: "
     @list_of_trains.delete_if {|train| train.number == departured_train}
-    puts "The train number #{departured_train} has departured from the #{self.station_name} station."
+    puts "The train number #{departured_train} has departured from the #{self.name} station."
   end
 
 end
@@ -47,27 +47,22 @@ class Route
   end
 
   def delete_station(station_to_delete)
-    @stations.delete_if{|intermediate_station| intermediate_station == station_to_delete}
+    @stations.delete{|intermediate_station| intermediate_station == station_to_delete}
     puts "Station #{station_to_delete} was deleted."
   end
 
-  def all_stations
-    puts @stations
-  end
 end
 #####################################################---Train---########################################################################
 
 class Train
 
-  attr_accessor :speed
-  attr_reader :number_of_carts, :type, :number, :route
+  attr_reader :number_of_carts, :type, :number, :route, :speed, :current_station
 
   def initialize (number, type, number_of_carts)
     @number = number
     @type = type
     @number_of_carts = number_of_carts
     @speed = 0
-    @current_station_number = nil
   end
 
   def start_moving
@@ -87,39 +82,27 @@ class Train
   end
 
   def delete_cart(carts_deleted)
-    if @speed == 0
+    if @speed == 0 && carts_deleted < @number_of_carts
       @number_of_carts -= carts_deleted
     else
-      puts "A cart can be deleted only when the train is stopped!"
+      puts "A cart can be deleted only when the train is stopped! Make sure that the train has enough carts."
     end
   end
 
   def add_route(route)
     @route = route
-    @current_station = route.stations.first.station_name
-    puts "Current station is #{@current_station}."
-    @route.stations.each_with_index do |station, index|
-      if station.station_name == @current_station
-        @current_station_number = index
-        puts "Number of the current station is #{@current_station_number}."
-      end
-    end
+    @current_station = @route.stations.first
+    @current_station_number = @route.stations.index(@current_station)
   end
 
   def move_forward
-    if @route.stations[@current_station_number + 1] != nil
-      @current_station = @route.stations[@current_station_number + 1].station_name
-    else
-      puts "The train is at the final destination!"
-    end
+    @current_station = @route.stations[@current_station_number + 1]
+    @current_station_number += 1
   end
 
   def move_back
-    if @route.stations[@current_station_number - 1] != nil
-      @current_station = @route.stations[@current_station_number - 1].station_name
-    else
-      puts "The train is at the starting station."
-    end
+    @current_station = @route.stations[@current_station_number - 1]
+    @current_station_number -= 1
   end
 
   def previous_station
